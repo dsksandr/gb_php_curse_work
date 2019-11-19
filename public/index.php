5f916401a644ee8046d2650c5fdf4385ae9c8611
dsksandr <zhilin_aleksandr@live.ru>
2019-11-19 14:50:54 +0700

    GB_PHP #7 - added try catch block

--- public/index.php
+++ public/index.php
@@ -2,23 +2,29 @@
 session_start();
 
 use app\controllers\Controller;
-use app\core\{Autoload, TwigRender, Request, Session};
+use app\core\{Autoload, Request, Session, TwigRender};
 
-include realpath("../config/config.php");
+try {
+    include realpath("../config/config.php");
 
-spl_autoload_register([new Autoload(), 'loadClass']);
+    spl_autoload_register([new Autoload(), 'loadClass']);
 
-$request = new Request();
+    $request = new Request();
 
-$controllerClass = CTRL_NAMESPACE . ucfirst($request->getControllerName()) . "Controller";
+    $controllerClass = CTRL_NAMESPACE . ucfirst($request->getControllerName()) . "Controller";
 
-if (class_exists($controllerClass)) {
-    /** @var Controller $controller */
-    $controller = new $controllerClass(
-        new Session(),
-        $request,
-        new TwigRender()
-    );
-} else {
-    echo "404 controller";
+    if (class_exists($controllerClass)) {
+        /** @var Controller $controller */
+        $controller = new $controllerClass(
+            new Session(),
+            $request,
+            new TwigRender()
+        );
+    } else {
+        $message = "Not found controller: {$controllerClass}. Url is incorrect ({$_SERVER['REQUEST_URI']}).";
+        throw new Exception($message);
+    }
+} catch (\Exception $exception) {
+    var_dump($exception);
 }
+
